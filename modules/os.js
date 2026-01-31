bf3967b4caf5a36c1faa5654fd4218ed48f546aa
Doston Jokhongirov <dostonjokhongirov@gmail.com>
2026-01-31 20:02:13 +0500

    os module

---
+++ modules/os.js
@@ -0,0 +1,23 @@
+import * as os from "node:os";
+
+// platform
+// console.log(os.platform());
+
+// information
+// console.log(os.cpus());
+
+// architecture 
+// console.log(os.arch());
+
+// free memory
+// console.log(os.freemem() / 1024);
+
+// total memory
+// console.log(os.totalmem() / 1024);
+
+// home direction
+// console.log(os.homedir());
+
+// console.log((os.freemem() / 1024 / 1024/ 1024).toFixed(2));
+
+// console.log((os.totalmem() / 1024 / 1024/ 1024).toFixed(2));
